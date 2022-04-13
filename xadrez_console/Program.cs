d57cdaf4f2e47724bb660ef6c11208066427ef34
Lucas-M01 <lucas.montenegro.n@outlook.com>
2022-04-13 12:07:05 -0300

    Melhorando implementacao do metodo imprimirTabuleiro

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -16,6 +16,8 @@ namespace xadrez_console
                 tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
 
+                tab.colocarPeca(new Rei(tab, Cor.Branco), new Posicao(3, 5));
+
                 Tela.imprimirTabuleiro(tab);
 
                 Console.WriteLine();
